7b63a19f6ef347ce413d300fd6923b37cf7ae96d
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-27 15:22:00 +0700

    feat: add navigate to detail history

--- src/pages/History/Transaction.jsx
+++ src/pages/History/Transaction.jsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { formatCurrency } from "../../utility/format.js";
 import PropTypes from "prop-types";
 
 function Transaction({ transaction }) {
+  const navigate = useNavigate();
+
   const { id, time, totalPrice } = transaction;
+
+  const handleGoToDetail = (id) => {
+    navigate(`/history/${id}`);
+  };
+
   return (
     <tr className="" key={id}>
       <td className="py-4 text-sm text-gray-600">{time}</td>
@@ -14,7 +22,10 @@ function Transaction({ transaction }) {
       </td>
 
       <td className="py-4 text-sm font-normal text-gray-500">
-        <button className="rounded-md bg-[#FF2351] px-2 py-1 text-white">
+        <button
+          className="rounded-md bg-[#FF2351] px-2 py-1 text-white"
+          onClick={() => handleGoToDetail(id)}
+        >
           Detail
         </button>
       </td>
