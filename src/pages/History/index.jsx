d6fa5cee4c080a42854cca4812266b0811a780d5
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-27 14:56:49 +0700

    feat: get data from api

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -1,21 +1,10 @@
+import axios from "axios";
+import useSWR from "swr";
+import { formatCurrency } from "../../utility/format";
+
 function History() {
-  const data = [
-    {
-      id: "62345231143",
-      date: "October 9, 2023",
-      totalPrice: "55.00",
-    },
-    {
-      id: "62916335791",
-      date: "September 27, 2023",
-      totalPrice: "28.00",
-    },
-    {
-      id: "62827168081",
-      date: "August 12, 2023",
-      totalPrice: "36.00",
-    },
-  ];
+  const getHistory = (url) => axios.get(url).then((res) => res.data);
+  const { data } = useSWR("http://localhost:3000/transactions", getHistory);
 
   return (
     <>
@@ -45,15 +34,18 @@ function History() {
             </thead>
 
             <tbody className="bg-white lg:border-gray-300">
-              {data.map(({ id, date, totalPrice }) => (
+              {data?.map(({ id, time, totalPrice }) => (
+                // <HistoryItem />
                 <tr className="" key={id}>
-                  <td className="py-4 text-sm text-gray-600">{date}</td>
+                  <td className="py-4 text-sm text-gray-600">{time}</td>
 
                   <td className="py-4 text-sm font-normal text-gray-600">
                     {id}
                   </td>
 
-                  <td className="py-4 text-sm text-gray-600">${totalPrice}</td>
+                  <td className="py-4 text-sm text-gray-600">
+                    {formatCurrency(totalPrice)}
+                  </td>
 
                   <td className="py-4 text-sm font-normal text-gray-500">
                     <button className="rounded-md bg-[#FF2351] px-2 py-1 text-white">
