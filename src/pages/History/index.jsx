b182c16a9e859ff36d1224451cc45f2a341b4e2d
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-27 15:55:42 +0700

    fix: adjust style & delete unnecessary line

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -5,42 +5,37 @@ import Transaction from "./Transaction";
 function History() {
   const getHistory = (url) => axios.get(url).then((res) => res.data);
   const { data } = useSWR("http://localhost:3000/transactions", getHistory);
-  console.log(data);
 
   return (
     <>
-      <h1 className="text-2xl font-bold uppercase">History</h1>
+      <h1 className="text-2xl font-bold uppercase">Transaction History</h1>
 
-      <div className="container">
-        <div className="mt-6 rounded-xl bg-white px-6 shadow">
-          <table className="w-full">
-            <thead className="border-b">
-              <tr className="">
-                <td className="py-4 text-sm font-semibold text-gray-800">
-                  Order Date
-                </td>
+      <div className="mt-10 rounded-xl bg-white px-6 shadow">
+        <table className="w-full">
+          <thead className="border-b">
+            <tr className="">
+              <td className="py-4 text-sm font-semibold text-gray-800">
+                Order Date
+              </td>
 
-                <td className="py-4 text-sm font-medium text-gray-500">
-                  Order ID
-                </td>
+              <td className="py-4 text-sm font-medium text-gray-500">
+                Order ID
+              </td>
 
-                <td className="py-4 text-sm font-medium text-gray-500">
-                  Total Price
-                </td>
+              <td className="py-4 text-sm font-medium text-gray-500">
+                Total Price
+              </td>
 
-                <td className="py-4 text-sm font-medium text-gray-500">
-                  Action
-                </td>
-              </tr>
-            </thead>
+              <td className="py-4 text-sm font-medium text-gray-500">Action</td>
+            </tr>
+          </thead>
 
-            <tbody className="bg-white lg:border-gray-300">
-              {data?.map(( transaction ) => (
-                <Transaction key={transaction.id} transaction={transaction}/>
-              ))}
-            </tbody>
-          </table>
-        </div>
+          <tbody className="bg-white lg:border-gray-300">
+            {data?.map((transaction) => (
+              <Transaction key={transaction.id} transaction={transaction} />
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
