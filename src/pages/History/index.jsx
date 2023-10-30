0d9a4138f3ddc8eebad056f33f2382669367aa89
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-30 17:46:21 +0700

    feat: replace redundant sort type to component

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import useSWR from "swr";
 import Transaction from "./Transaction";
 import { useState } from "react";
+import SortHeader from "./SortHeader";
 
 function History() {
   const getHistory = (url) => axios.get(url).then((res) => res.data);
@@ -44,83 +45,23 @@ function History() {
         <table className="w-full">
           <thead className="border-b">
             <tr className="">
-              <td
-                className="flex cursor-pointer py-4 text-sm font-semibold text-gray-800 hover:text-[#e81e48]"
-                onClick={handleSortClick("date")}
-              >
-                {/* Down Arrow */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`h-5 w-5 ${sortDate ? "block" : "hidden"}`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
-                  />
-                </svg>
-                {/* Up Arrow */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`h-5 w-5 ${sortDate ? "hidden" : "block"}`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
-                  />
-                </svg>
-                Order Date
-              </td>
+              <SortHeader
+                label="Order Date"
+                sortType="date"
+                sortDirection={sortDate ? "desc" : "asc"}
+                onClick={handleSortClick}
+              />
 
               <td className="py-4 text-sm font-medium text-gray-500">
                 Order ID
               </td>
 
-              <td
-                className="flex cursor-pointer py-4 text-sm font-semibold text-gray-800 hover:text-[#e81e48]"
-                onClick={handleSortClick("price")}
-              >
-                {/* Up Arrow */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`h-5 w-5 ${sortPrice ? "hidden" : "block"}`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
-                  />
-                </svg>
-                {/* Down Arrow */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`h-5 w-5 ${sortPrice ? "block" : "hidden"}`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
-                  />
-                </svg>
-                Total Price
-              </td>
+              <SortHeader
+                label="Total Price"
+                sortType="price"
+                sortDirection={sortPrice ? "desc" : "asc"}
+                onClick={handleSortClick}
+              />
 
               <td className="py-4 text-sm font-medium text-gray-500">Action</td>
             </tr>
