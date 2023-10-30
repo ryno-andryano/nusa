f068d0715381e03870358ba0fd6c3ef8013f6771
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-10-30 15:55:52 +0700

    fix: reorder icon

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -42,21 +42,6 @@ function History() {
                 className="flex cursor-pointer py-4 text-sm font-semibold text-gray-800 hover:text-[#e81e48]"
                 onClick={handleSortChange}
               >
-                {/* Down Arrow */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`h-5 w-5 ${sortLatest ? "hidden" : "block"}`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
-                  />
-                </svg>
                 {/* Up Arrow */}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -70,6 +55,21 @@ function History() {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
+                  />
+                </svg>
+                {/* Down Arrow */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={`h-5 w-5 ${sortLatest ? "hidden" : "block"}`}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
                   />
                 </svg>
                 Order Date
